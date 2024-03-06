7a40a68f4122e5391b1b52df2e47318995bdd3d3
Mohammad-Rnjbr <mohammadranjbar.mr81@gmail.com>
2024-03-06 11:50:53 +0330

    insert jpa properties in application.properties file and create repository for library entity

---
+++ src/main/java/com/application/repository/LibraryRepository.java
@@ -0,0 +1,9 @@
+package com.application.repository;
+
+import com.application.entity.Library;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibraryRepository extends JpaRepository<Library,String> {
+}
